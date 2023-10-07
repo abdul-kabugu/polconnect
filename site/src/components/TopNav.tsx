4f603b851130f6d5f3e835368dc82e0141fe04a1
Abdul-kabugu <kabuguabdul2@gmail.com>
2023-10-07 18:21:15 +0300

    Feat : added show chain prop

--- site/src/components/TopNav.tsx
+++ site/src/components/TopNav.tsx
@@ -4,7 +4,7 @@ export default function TopNav() {
   return (
     <div className='flex justify-between bg-black/80 sticky top-0  items-center px-3 py-2 h-[60px] max-w-screen-xl mx-auto'>
       <h1 className='font-semibold'>PolconnectKit</h1>
-      <ConnectButton  label='Connect Wallet' backGround='blue' />
+      <ConnectButton  label='Connect Wallet' backGround='blue' showChain={true} />
     </div>
   )
 }
